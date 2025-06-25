e63ff77a67e214162123b4a686a7a71317079a61
sintijab <sintija649@gmail.com>
2025-06-25 23:57:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/health', (req, res) => {
 io.on('connection', (socket) => {
   const getOffers = async (country) => {
     try {
-      const response = await fetch(`raspy-dew-8966.ploomber.app/api/scrape-jobs?country=${country}`);
+      const response = await fetch(`https://raspy-dew-8966.ploomber.app/api/scrape-jobs?country=${country}`);
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
